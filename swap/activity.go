7a0613f6342b8d19d665c2408d75f7323f514b1b
hariHp1532 <82503003+hariHp1532@users.noreply.github.com>
2021-04-26 11:18:36 +0530

    swap

--- swap/activity.go
+++ swap/activity.go
@@ -32,6 +32,7 @@ func (a *Activity) Metadata() *activity.Metadata {
 // Eval implements api.Activity.Eval - Logs the Message
 func (a *Activity) Eval(ctx activity.Context) (done bool, err error) {
 
+	var out1,out2 int
 	input := &Input{}
 
 	err = ctx.GetInputObject(input) //GetInputObject gets all the activity input as the specified object.
