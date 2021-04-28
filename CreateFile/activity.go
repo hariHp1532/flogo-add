eaa32792ff0cc67cf5136c5104c7381de46c0058
hariHp1532 <82503003+hariHp1532@users.noreply.github.com>
2021-04-29 00:19:05 +0530

    CreateFile

---
+++ CreateFile/activity.go
@@ -0,0 +1,63 @@
+package CreateFile
+
+import (
+	"log"
+	"os"
+
+	"github.com/project-flogo/core/activity"
+)
+
+func init() {
+	_ = activity.Register(&Activity{}) //activity.Register(&Activity{}, New) to create instances using factory method 'New'
+}
+
+var activityMd = activity.ToMetadata(&Input, &Output)
+
+//var activityMd = activity.ToMetadata(&Input{}, &Output{})
+
+//New optional factory method, should be used if one activity instance per configuration is desired
+func New(ctx activity.InitContext) (activity.Activity, error) {
+
+	act := &Activity{} //add aSetting to instance
+
+	return act, nil
+}
+
+// Activity is an sample Activity that can be used as a base to create a custom activity
+type Activity struct {
+}
+
+// Metadata returns the activity's metadata
+func (a *Activity) Metadata() *activity.Metadata {
+	return activityMd
+}
+
+// Eval implements api.Activity.Eval - Logs the Message
+func (a *Activity) Eval(ctx activity.Context) (done bool, err error) {
+
+	var file, out string
+	input := &Input{}
+
+	err = ctx.GetInputObject(input) //GetInputObject gets all the activity input as the specified object.
+	if err != nil {
+		return true, err
+	}
+	file = input.FileName
+
+	emptyFile, err := os.Create(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(emptyFile)
+
+	out = file
+
+	output := &Output{Output: out}
+
+	err = ctx.SetOutputObject(output)
+	if err != nil {
+		return true, err
+	}
+
+	return true, nil
+}
---
+++ CreateFile/metadata.go
@@ -0,0 +1,43 @@
+package CreateFile
+
+import (
+	"github.com/project-flogo/core/data/coerce"
+	//"github.com/spf13/cast"
+)
+
+type Input struct {
+	FileName string `md:"FileName.required"`
+}
+
+func (r *Input) FromMap(values map[string]interface{}) error {
+
+	Val1, _ := coerce.ToString(values["FileName"])
+	r.FileName = Val1
+}
+
+func (r *Input) ToMap() map[string]interface{} {
+	return map[string]interface{}{
+		"FileName": r.FileName,
+	}
+}
+
+type Output struct {
+	Output int `md:"Output"`
+}
+
+func (o *Output) FromMap(values map[string]interface{}) error {
+
+	var err error
+	o.Output1, err = coerce.ToInt(values["Output"])
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (o *Output) ToMap() map[string]interface{} {
+	return map[string]interface{}{
+		"Output": o.Output
+	}
+}
