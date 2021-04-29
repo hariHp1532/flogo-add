70c013b7305e22b0969435a569b053bf80bbe3a8
hariHp1532 <82503003+hariHp1532@users.noreply.github.com>
2021-04-29 19:30:47 +0530

    filter

--- filter/metadata.go
+++ filter/metadata.go
@@ -17,15 +17,12 @@ func (r *Input) FromMap(values map[string]interface{}) error {
 
 	Val1, _ := coerce.ToInt(values["Num1"])
 	r.Num1 = Val1
-	fmt.Println("num1 value is: ", values["Num1"])
 
 	Val2, _ := coerce.ToInt(values["Num2"])
 	r.Num2 = Val2
-	fmt.Println("num2 value is: ", values["Num2"])
 
 	Val3, _ := coerce.ToString(values["Operation"])
 	r.Operation = Val3
-	fmt.Println("operation value is: ", values["Operation"])
 
 	return nil
 }
@@ -45,7 +42,7 @@ type Output struct {
 func (o *Output) FromMap(values map[string]interface{}) error {
 	strVal, _ := coerce.ToInt(values["Output"])
 	o.Output = strVal
-	//fmt.Println("output value is: ", values["Output"])
+	
 	return nil
 }
 
