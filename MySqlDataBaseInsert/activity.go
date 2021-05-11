2d356a28bfda1f1f2930e2389a33b238a3bf3f7f
hariHp1532 <82503003+hariHp1532@users.noreply.github.com>
2021-05-11 18:35:12 +0530

    hari testing shalini's use case

---
+++ MySqlDataBaseInsert/activity.go
@@ -0,0 +1,86 @@
+package MySqlDataBaseInsert
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/project-flogo/core/activity"
+)
+
+func init() {
+	_ = activity.Register(&Activity{}) //activity.Register(&Activity{}, New) to create instances using factory method 'New'
+}
+
+var activityMd = activity.ToMetadata(&Input{}, &Output{})
+
+//var activityMd = activity.ToMetadata(&Input{}, &Output{})
+
+//New optional factory method, should be used if one activity instance per configuration is desired
+func New(ctx activity.InitContext) (activity.Activity, error) {
+
+	act := &Activity{} //add aSetting to instance
+
+	return act, nil
+}
+
+// Activity is an sample Activity that can be used as a base to create a custom activity
+type Activity struct {
+}
+
+// Metadata returns the activity's metadata
+func (a *Activity) Metadata() *activity.Metadata {
+	return activityMd
+}
+
+// Eval implements api.Activity.Eval - Logs the Message
+func (a *Activity) Eval(ctx activity.Context) (done bool, err error) {
+
+	var DBType, userName, password, dataBase, cred, table, value, out string
+	input := &Input{}
+
+	err = ctx.GetInputObject(input) //GetInputObject gets all the activity input as the specified object.
+	if err != nil {
+		return true, err
+	}
+	DBType = "mysql"
+	userName = input.UserName
+	password = input.PassWord
+	dataBase = input.DataBase
+	cred = userName + ":" + password + "@tcp(127.0.0.1:3306)/" + dataBase
+
+	db, err := sql.Open(DBType, cred)
+	if err != nil {
+		//out = "error"
+		fmt.Println("connection error")
+		panic(err.Error())
+	}
+	//out = "success"
+	fmt.Println("connection success")
+
+	table = input.Table
+	value = input.Value
+
+	sqlo := "INSERT INTO " + table + "(id, message) VALUES (1, '" + value + "');"
+	fmt.Println(sqlo)
+	res, errr := db.Exec(sqlo)
+
+	if errr != nil {
+		panic(errr.Error())
+	}
+	lastId, errr := res.LastInsertId()
+
+	if errr != nil {
+		log.Fatal(errr)
+	}
+	fmt.Printf("The last inserted row id: %d\n", lastId)
+	out = lastId
+	output := &Output{Output: out}
+	err = ctx.SetOutputObject(output)
+	if err != nil {
+		return true, err
+	}
+
+	return true, nil
+}
---
+++ MySqlDataBaseInsert/metadata.go
@@ -0,0 +1,65 @@
+package MySqlDataBaseInsert
+
+import (
+	"github.com/project-flogo/core/data/coerce"
+	//"github.com/spf13/cast"
+)
+
+type Input struct {
+	UserName string `md:"UserName.required"`
+	PassWord string `md:"PassWord.required"`
+	DataBase string `md:"DataBase.required"`
+	Table    string `md:"Table.required"`
+	Value    string `md:"Value.required"`
+}
+
+func (r *Input) FromMap(values map[string]interface{}) error {
+
+	Val1, _ := coerce.ToString(values["UserName"])
+	r.UserName = Val1
+
+	Val2, _ := coerce.ToString(values["PassWord"])
+	r.PassWord = Val2
+
+	Val3, _ := coerce.ToString(values["DataBase"])
+	r.DataBase = Val3
+
+	Val4, _ := coerce.ToString(values["Table"])
+	r.Table = Val4
+
+	Val5, _ := coerce.ToString(values["Value"])
+	r.Value = Val4
+
+	return nil
+}
+
+func (r *Input) ToMap() map[string]interface{} {
+	return map[string]interface{}{
+		"UserName": r.UserName,
+		"PassWord": r.PassWord,
+		"DataBase": r.DataBase,
+		"Table":    r.Table,
+		"Value":    r.Value,
+	}
+}
+
+type Output struct {
+	Output string `md:"Output"`
+}
+
+func (o *Output) FromMap(values map[string]interface{}) error {
+
+	var err error
+	o.Output, err = coerce.ToString(values["Output"])
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (o *Output) ToMap() map[string]interface{} {
+	return map[string]interface{}{
+		"Output": o.Output,
+	}
+}
